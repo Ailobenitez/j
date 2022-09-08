a9dc116af027d703b77724d3b2bbd4d3394ae5a7
Ailobenitez <110872183+Ailobenitez@users.noreply.github.com>
2022-09-08 00:46:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ app.use(express.static(path.resolve(__dirname, "./public")
 app.get("/", (req, res)=> {
   res.sendFile(path.resolve(__dirname, "./public"))
 });
-app.listen(process.env.PORT||3000,function(){git 
+app.listen(process.env.PORT||3000, function(){git 
   console.log('servidor corriendo en puerto 3000')
 })
