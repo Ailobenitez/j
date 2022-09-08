3986521bb70a350b0311314e3de708df85003637
Ailobenitez <110872183+Ailobenitez@users.noreply.github.com>
2022-09-08 00:34:21 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require ("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static("public"));
+
+
+
+;app.get("/", (req, res)=> {
+    res.sendFile(path.resolve(__dirname, "./index.html"))
+});
+
+app.get("/", (req, res)=> {
+    res.sendFile(path.resolve(__dirname, "./apariencia.css"))
+});
+app.use(express.static(path.resolve(__dirname, "./public")
+))
+app.get("/", (req, res)=> {
+  res.sendFile(path.resolve(__dirname, "./public"))
+});
+app.listen(process.env.PORT||3000,function(){git 
+  console.log('servidor corriendo en puerto 3000')
+})
